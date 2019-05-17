587315c3114840c653cdb743e791209497cbc9de
Reina Ewing <reina.castro3@gmail.com>
2019-05-17 15:31:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
     array.join(" ")
   elsif array.length == 2
     array.join(" and ")
-  else array.length >= 3
-    array[-1] = "and #{array -1 }"
-    array.join(", ")
+    elsif array.length >= 3
+      array[-1] = "and #{array [-1]}"
+       array.join(", ")
 end 
 end
 
